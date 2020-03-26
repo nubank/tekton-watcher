0d72da6731546c5461d26f1ef3c8f4ca7fdc4d15
Alan Ghelardi <alan.ghelardi@gmail.com>
2020-03-26 12:07:45 -0300

    Improve config and write tests.

--- src/tekton_watcher/config.clj
+++ src/tekton_watcher/config.clj
@@ -1,44 +1,41 @@
 (ns tekton-watcher.config
   (:require [clojure.data :as data]
-            [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.tools.logging :as log]
-            [tekton-watcher.misc :as misc]))
-
-(defn- read-edn
-  "Reads data in EDN format from the input in question."
-  [input]
-  (edn/read-string (slurp input)))
+            [tekton-watcher.misc :as misc])
+  (:import clojure.lang.Var
+           java.io.File))
 
 (defn read-resource
   "Reads config data from the resource in question."
-  [^String file-name]
-  (let [input-stream (io/input-stream (io/resource (str "tekton_watcher/" file-name)))]
+  [^String resources-path]
+  (let [input-stream (io/input-stream (io/resource (str "tekton_watcher/" resources-path)))]
     (when input-stream
-      (read-edn input-stream))))
-
-(defn- file-exists?
-  "Returns true if the file in question exists or false otherwise."
-  [file]
-  (.exists file))
+      (misc/read-edn input-stream))))
 
 (defn read-file
-  [^String file-name]
-  (let [file (io/file file-name)]
-    (when (file-exists? file)
-      (read-edn file))))
+  "Reads config data from the file in question."
+  [^String file-path]
+  (let [^File file (io/file file-path)]
+    (when (misc/file-exists? file)
+      (misc/read-edn file))))
 
 (defn- log-kv
+  "Logs keys and values in the config data."
   [source-name current-config loaded-config]
   (let [[_ overrides] (data/diff current-config loaded-config)]
     (doseq [[k v] overrides]
       (log/info :overriding-config :source source-name :key k :value v))))
 
 (defn- wrap-reader
-  [reader-var]
+  "Given a var that references a no args function that reads config data
+  from an arbitrary source, returns a function that takes the current
+  config data and performs a shallow merge of the current config into
+  the config read by the reader function."
+  [^Var reader-var]
   (fn [current-config]
     (let [source-name   (subs (str reader-var) 2)
-          loaded-config (@reader-var)]
+          loaded-config (@@reader-var)]
       (if-not loaded-config
         (do (log/info :config :source source-name :message :no-config-found)
             current-config)
@@ -46,31 +43,51 @@
             (merge current-config loaded-config))))))
 
 (defn- read-waterfall
+  "Reads the config data from the supplied sources."
   [& sources]
-  (reduce #(%2 %1) {}
-          (map #(wrap-reader %) sources)))
+  (reduce (fn [config reader]
+            (reader config))
+          {} (map #(wrap-reader %) sources)))
 
-(defn- read-github-oauth-token
-  [{:github.oauth-token/keys [path] :as config}]
-  (let [file (io/file path)]
-    (if (file-exists? file)
-      (assoc config :github/oauth-token (slurp file))
-      (throw (ex-info "Github oauth token not found. Did you forget to create a secret named `github-statuses-updater`?"
-                      {:path path})))))
-
-(defn render-config
+(defn- render-config
+  "Traverses config values and replaces templates (e.g. {key}) with the
+  corresponding values supplied through the config data."
   [config]
   (misc/map-vals #(if-not (string? %)
                     %
                     (misc/render % config))
                  config))
 
-(def resource (partial read-resource "config.edn"))
+(defn read-github-oauth-token
+  "Reads the Github oauth token used for updating status checks.
 
-(def config-map (partial read-file "/etc/tekton-watcher/config.edn"))
+  Throws an exception if the secret can't be found."
+  [^String file-path]
+  (let [^File file (io/file file-path)]
+    (if (misc/file-exists? file)
+      (slurp file)
+      (throw (ex-info "Github oauth token not found. Did you forget to create a secret named `github-statuses-updater`?"
+                      {:path file-path})))))
+
+(defn- add-github-oauth-token
+  "Given a config data, reads the Github oauth token used to make
+  requests to the statuses API and assoc's it into the config data."
+  [{:github.oauth-token/keys [path] :as config}]
+  (assoc config
+         :github/oauth-token (read-github-oauth-token path)))
+
+(def resource
+  "Reads config data from tekton-watcher/config.edn on the classpath."
+  (delay (partial read-resource "config.edn")))
+
+(def config-map
+  "Reads config data from a config map mounted at
+  /etc/tekton-watcher/config.edn."
+  (delay (partial read-file "/etc/tekton-watcher/config.edn")))
 
 (defn read-config
+  "Reads config data from the supported sources (currently resources and config map)."
   []
   (-> (read-waterfall #'resource #'config-map)
       render-config
-      read-github-oauth-token))
+      add-github-oauth-token))
---
+++ test/tekton_watcher/config_test.clj
@@ -0,0 +1,69 @@
+(ns tekton-watcher.config-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [matcher-combinators.test :refer [match?]]
+            [mockfn.macros :refer [providing]]
+            [tekton-watcher.config :as config]
+            [tekton-watcher.misc :as misc]))
+
+(deftest read-resource-test
+  (testing "reads config data from the supplied resource file"
+    (is (not (empty?
+              (config/read-resource "config.edn"))))))
+
+(deftest read-file-test
+  (testing "reads config data from the supplied file path"
+    (providing [(misc/file-exists? (io/file "/etc/tekton-watcher/config.edn")) true
+                (slurp (io/file "/etc/tekton-watcher/config.edn")) "{:tekton-watcher/key \"value\"}"]
+               (is (= {:tekton-watcher/key "value"}
+                      (config/read-file "/etc/tekton-watcher/config.edn")))))
+
+  (testing "returns nil when there is no file at the supplied path"
+    (providing [(misc/file-exists? (io/file "/etc/tekton-watcher/config.edn")) false]
+               (is (nil? (config/read-file "/etc/tekton-watcher/config.edn"))))))
+
+(deftest read-github-oauth-token-test
+  (testing "reads the github oauth token"
+    (providing [(misc/file-exists? (io/file "/etc/github-statuses-updater/oauth-token")) true
+                (slurp (io/file "/etc/github-statuses-updater/oauth-token")) "token"]
+               (is (= "token"
+                      (config/read-github-oauth-token "/etc/github-statuses-updater/oauth-token"))))))
+
+(testing "throws an exception when the secret can't be found"
+  (providing [(misc/file-exists? (io/file "/etc/github-statuses-updater/oauth-token")) false]
+             (is (thrown? clojure.lang.ExceptionInfo
+                          (config/read-github-oauth-token "/etc./github-statuses-updater/oauth-token")))))
+
+(deftest read-config-test
+  (providing [(config/read-resource "config.edn") {:api.server/url          "http://localhost:8080"
+                                                   :github.oauth-token/path "/etc/github-statuses-updater/oauth-token"}
+              (config/read-file "/etc/tekton-watcher/config.edn") {:api.server/url   "http://localhost:9000"
+                                                                   :tekton.api/path  "/apis/tekton.dev/v1alpha1"
+                                                                   :tekton.api/url
+                                                                   "{api.server/url}{tekton.api/path}/namespaces/{tekton/namespace}"
+                                                                   :tekton/namespace "default"}
+              (config/read-github-oauth-token "/etc/github-statuses-updater/oauth-token") "token"]
+             (let [config-data (config/read-config)]
+
+               (testing "config data contains values from the config.edn resource file"
+                 (is (match? {:github.oauth-token/path "/etc/github-statuses-updater/oauth-token"}
+                             config-data)))
+
+               (testing "config data contains values from the mounted config map"
+                 (is (match? {:tekton.api/path   "/apis/tekton.dev/v1alpha1"
+                              :tekton/namespace "default"}
+                             config-data)))
+
+               (testing "values supplied through the config map override values from the resource file"
+                 (is (match? {:api.server/url "http://localhost:9000"}
+                             config-data)))
+
+               (testing "values containing templates are expanded according to
+               other values defined in the config data"
+                 (is (match? {:tekton.api/url
+                              "http://localhost:9000/apis/tekton.dev/v1alpha1/namespaces/default"}
+                             config-data)))
+
+               (testing "config data contains the Github oauth token"
+                 (is (match? {:github/oauth-token "token"}
+                             config-data))))))
